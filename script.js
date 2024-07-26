62d3a6278e033b477c123253736e3b8394e3c5d7
SriMoorthy <srimoorthy27@gmail.com>
2024-07-26 12:59:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+let bold_items = document.getElementsByTagName('strong')
+
 function highlight() {
     //Write your code here
+	for (let i = 0; i < bold_items.length; i++) {
+		bold_items[i].style.color = 'green'
+	}
 
 
 }
@@ -7,6 +12,7 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
-
-    
+	for (let i = 0; i < bold_items.length; i++) {
+		bold_items[i].style.color = 'black'
+	}
 }
